04ab7f551ba74f7dcb2424438c088f579dec2745
yiershan <511176294@qq.com>
2018-04-18 10:11:18 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MZC-Ng-Api
+
 个人博客系统，从后台api取数据的angular封装
+
+## License
+
+请查看 [MIT license](./LICENSE).
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+
+export class MzcNgApi{
+    private name: string;
+    constructor() {
+        this.name = "MzcNgApi";
+    }
+}
